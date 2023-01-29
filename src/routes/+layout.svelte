2a4cdea4bf02fb47ec43f65390fa40fb9c475de2
JhessReyes <jhessaureyes@gmail.com>
2023-01-28 22:12:55 -0600

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.css';
+	import Header from '../routes/components/Header/index.svelte';
 </script>
 
 <div class="w-full h-full">
+	<Header />
 	<main>
 		<slot />
 	</main>
---
+++ src/routes/components/Header/index.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<div class="mx-auto h-24 navbar">
+		<div class="flex-1 h-24">
+			<a href="https://kit.svelte.dev"> logo </a>
+		</div>
+
+		<nav>
+			<div class="flex-none just">
+				<ul class="list-none sm:flex hidden justify-end items-center flex-1">
+					<li
+						class="text-[16px] cursor-pointer"
+						aria-current={$page.url.pathname === '/' ? 'page' : undefined}
+					>
+						<a href="/">INICIO</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="ml-20">
+			<ul class="list-none sm:flex hidden justify-end items-center flex-1">
+				<li
+					class="text-[16px] cursor-pointer"
+					aria-current={$page.url.pathname === '/' ? 'page' : undefined}
+				>
+					<a href="/">user</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</header>
