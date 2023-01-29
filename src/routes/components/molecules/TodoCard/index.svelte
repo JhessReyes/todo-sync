5b233261139f91eea933611284b04844dd4699e9
JhessReyes <jhessaureyes@gmail.com>
2023-01-28 22:22:25 -0600

    todo card

---
+++ src/routes/components/molecules/TodoCard/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title = 'TITLE';
+</script>
+
+<div class="rounded-xl shadow-xl md:w-50 h-full">
+	<div class="card-body">
+		<h2 class="card-title text-primary capitalize">{title}</h2>
+	</div>
+</div>
