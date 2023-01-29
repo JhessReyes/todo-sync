99b3b54f078fbde42e908617874883e037a84b48
JhessReyes <jhessaureyes@gmail.com>
2023-01-29 14:18:58 -0600

    todo card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,11 @@
 
 <section id="home">
 	<div class="flex">
-		<div class="overflow-auto">
+		<div class="overflow-auto rounded-xl shadow-xl mx-52 md:w-50 h-full">
 			<h1>PLANEADO</h1>
 			<HomeLayout />
 		</div>
-		<div class="overflow-auto">
+		<div class="overflow-auto rounded-xl shadow-xl md:w-50 h-full">
 			<h1>TERMINADO</h1>
 			<HomeLayout />
 		</div>
--- src/routes/components/molecules/TodoCard/index.svelte
+++ src/routes/components/molecules/TodoCard/index.svelte
@@ -1,28 +1,11 @@
 <script lang="ts">
-	export let title = 'TITLE';
 	interface Todo {
 		task: string;
 		isComplete: boolean;
 		createdAt: string;
 	}
 
-	let todos: Todo[] = [
-		{
-			task: 'tasdgs',
-			isComplete: false,
-			createdAt: '2021-08-06'
-		},
-		{
-			task: 'tasdgs',
-			isComplete: true,
-			createdAt: '2021-08-06'
-		},
-		{
-			task: 'tasdgs',
-			isComplete: true,
-			createdAt: '2021-08-06'
-		}
-	];
+	let todos: Todo[] = [];
 
 	let todo: Todo = {
 		task: '',
@@ -30,23 +13,43 @@
 		createdAt: new Date().toISOString()
 	};
 
+	const resetTodo = (): void => {
+		todo = {
+			task: '',
+			isComplete: false,
+			createdAt: new Date().toISOString()
+		};
+	};
 	const addTodo = (): void => {
 		todos = [...todos, todo];
+		resetTodo();
 	};
 
+	$: console.log(todo.task);
 	$: console.table(todos);
 </script>
 
 <div class="rounded-xl shadow-xl md:w-50 h-full">
 	<input type="text" aria-placeholder="Agrega un plan" bind:value={todo.task} />
-	<button on:click={addTodo}>Add</button>
-
+	{#if todo.task != ''}
+		<button on:click={addTodo}>Add</button>
+	{/if}
 	<ul>
-		{#each todos as todo}
-			<li class:complete={todo.isComplete}>{todo.task}</li>
+		{#each todos as t}
+			<li class={t.isComplete ? 'line-through italic' : ''}>
+				<div class="form-control">
+					<label class="cursor-pointer label">
+						<div class="card-body">
+							<h2 class="card-title text-primary capitalize">{t.task}</h2>
+							<input
+								type="checkbox"
+								bind:checked={t.isComplete}
+								class="checkbox checkbox-secondary"
+							/>
+						</div>
+					</label>
+				</div>
+			</li>
 		{/each}
 	</ul>
-	<div class="card-body">
-		<h2 class="card-title text-primary capitalize">{title}</h2>
-	</div>
 </div>
