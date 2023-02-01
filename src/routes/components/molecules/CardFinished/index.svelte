edd467a746815642e56f76c3b5696eb6b6a3be42
JhessReyes <jhessaureyes@gmail.com>
2023-02-01 03:09:31 -0600

    CardFinished

---
+++ src/routes/components/molecules/CardFinished/index.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let t: any = [];
+</script>
+
+{#if t}
+	<div class="form-control">
+		<label class="cursor-pointer label">
+			<div class="card-body">
+				<div class="flex">
+					<input disabled type="checkbox" checked={true} class="checkbox checkbox-secondary mx-2" />
+					<div class={true ? 'line-through italic' : ''}>
+						<h2 class="card-title text-primary capitalize">{t.summary}</h2>
+					</div>
+				</div>
+				<div class="justify-end">
+					<div class="flex">
+						<strong class="my-2 mx-2">Inicio: </strong>
+						<h4 class="text-primary text-[16px] italic py-2">
+							{t.start.date || t.start.dateTime}
+						</h4>
+					</div>
+					<div class="flex">
+						<strong class="my-2 mx-2">Fin: </strong>
+						<h4 class="text-primary text-[16px] italic py-2">
+							{t.end.date || t.end.dateTime}
+						</h4>
+					</div>
+				</div>
+			</div>
+		</label>
+	</div>
+{/if}
--- src/routes/components/templates/FinishedLayout/index.svelte
+++ src/routes/components/templates/FinishedLayout/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import TodoCard from '../../molecules/TodoCard/index.svelte';
-	import Card from '../../molecules/Card/index.svelte';
+	import CardFinished from '../../molecules/CardFinished/index.svelte';
 	import SplashLoading from '../../molecules/SplashLoading/index.svelte';
 	import { getEventsFinished } from '../../../providers/GoogleCalendar';
 	import { onMount } from 'svelte';
@@ -23,44 +22,11 @@
 			<progress class="progress w-full h-2" />
 		{:then res}
 			{#each res.result.items as t}
-				<Card {t} />
+				<CardFinished {t} />
 			{/each}
-			<!-- {#each res.result.items as t}
-				<div class="form-control">
-					<label class="cursor-pointer label">
-						<div class="card-body">
-							<div class="flex">
-								<input type="checkbox" checked={true} class="checkbox checkbox-secondary mx-2" />
-								<div class={true ? 'line-through italic' : ''}>
-									<h2 class="card-title text-primary capitalize">{t.summary}</h2>
-								</div>
-							</div>
-							<div class="justify-end">
-								<div class="flex">
-									<strong class="my-2 mx-2">Inicio: </strong>
-									<h4 class="text-primary text-[16px] italic py-2">
-										{t.start.date || t.start.dateTime}
-									</h4>
-								</div>
-								<div class="flex">
-									<strong class="my-2 mx-2">Fin: </strong>
-									<h4 class="text-primary text-[16px] italic py-2">
-										{t.end.date || t.end.dateTime}
-									</h4>
-								</div>
-							</div>
-							{#if t.completeAt == ''}
-								<button class="btn btn-secondary">Agregar Fecha de Vencimiento</button>
-							{/if}
-						</div>
-					</label>
-				</div>
-			{/each} -->
 		{:catch error}
 			<p style="color: red">{error.message}</p>
 		{/await}
-
-		<!-- 	<TodoCard {allEvents} /> -->
 	</div>
 {:else}
 	<SplashLoading />
